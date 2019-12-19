6d3537e2e9012e1dd9a48cd021af7d4308f12324
mydadisalive <mydadisalive@gmail.com>
2019-12-19 14:54:18 +0000

    some fixes

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -5,6 +5,7 @@ from botocore.exceptions import ClientError
 
 session = boto3.Session()
 s3 = session.resource('s3')
+print(session.region_name)
 
 @click.group()
 def cli():
@@ -28,35 +29,33 @@ def list_bucket_objects(bucket):
 @click.argument('bucket')
 def setup_bucket(bucket):
     "Create and configure S3 bucket"
-    try:
-        s3_bucket = s3.create_bucket(
-            Bucket=bucket
-            #CreateBucketConfiguration={'LocationConstraint': session.region_name}
-        )
-    except ClientError as e:
-        if e.response['Error']['Code']== 'BucketAlreadyOwnedByYou':
-            s3_bucket = s3.Bucket(bucket)
-        else:
-            raise e
+    s3_bucket = None
+    #try:
+    s3_bucket = s3.create_bucket(
+    #except ClientError as e:
+    #    if e.response['Error']['Code']== 'BucketAlreadyOwnedByYou':
+    #        print(e)
+    #        s3_bucket = s3.Bucket(bucket)
+    #    else:
+    #        raise e
 
 
     policy = """
-{
-  "Version": "2012-10-17",
-  "Statement": [{
-  "Sid": "PublicReadGetObject",
-  "Effect": "Allow",
-  "Principal": "*",
-    "Action": ["s3:GetObject"],
-    "Resource": ["arn:aws:s3:::%s/*"]
-  }]
-}
+    {
+      "Version": "2012-10-17",
+      "Statement": [{
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+        "Action": ["s3:GetObject"],
+        "Resource": ["arn:aws:s3:::%s/*"]
+      }]
+    }
     """ % s3_bucket.name
-
     policy = policy.strip()
+
     pol = s3_bucket.Policy()
     pol.put(Policy=policy)
-
     
     ws = s3_bucket.Website()
     ws.put(WebsiteConfiguration={
