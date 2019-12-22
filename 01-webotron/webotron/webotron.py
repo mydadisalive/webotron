a8c0d497ab918cd1aec3004dc11ae0a85c79935e
mydadisalive <mydadisalive@gmail.com>
2019-12-22 13:27:13 +0000

    sync direcory to s3

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -2,10 +2,11 @@ import sys
 import boto3
 import click 
 from botocore.exceptions import ClientError
+from pathlib import Path
+import mimetypes
 
 session = boto3.Session()
 s3 = session.resource('s3')
-print(session.region_name)
 
 @click.group()
 def cli():
@@ -32,6 +33,8 @@ def setup_bucket(bucket):
     s3_bucket = None
     #try:
     s3_bucket = s3.create_bucket(
+            Bucket=bucket,
+            )
     #except ClientError as e:
     #    if e.response['Error']['Code']== 'BucketAlreadyOwnedByYou':
     #        print(e)
@@ -52,7 +55,7 @@ def setup_bucket(bucket):
       }]
     }
     """ % s3_bucket.name
-    policy = policy.strip()
+    policy=policy.strip()
 
     pol = s3_bucket.Policy()
     pol.put(Policy=policy)
@@ -68,6 +71,31 @@ def setup_bucket(bucket):
     })
 
     return
+
+def upload_file(s3_bucket, path, key):
+    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+    s3_bucket.upload_file(
+            path,
+            key,
+            ExtraArgs={
+                'ContentType': 'text/html',
+            }
+    )
+@cli.command('sync')
+@click.argument('pathname', type=click.Path(exists=True))
+@click.argument('bucket')
+def sync(pathname, bucket):
+    "Sync contents of PATHNAME to BUCKET"
+    s3_bucket = s3.Bucket(bucket)
+    
+    root = Path(pathname).expanduser().resolve()
+
+    def handle_directory(target):
+        for p in target.iterdir():
+            if p.is_dir(): handle_directory(p)
+            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root)))
+
+    handle_directory(root)
 
 if __name__ == "__main__":
     cli()
