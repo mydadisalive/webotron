db35af19e4a78ed82cfb1bde8aab27bf3d3c2d7e
mydadisalive <mydadisalive@gmail.com>
2019-12-27 09:03:02 +0000

    Changes to ipython start script

--- 01-webotron/ipythonsession.py
+++ 01-webotron/ipythonsession.py
@@ -1,4 +1,8 @@
 # coding: utf-8
 import boto3
+from pprint import pprint
+
+client = boto3.client('cloudfront')
+paginator = client.get_paginator('list_distributions')
 session = boto3.Session(profile_name = 'automation')
 s3 = session.resource('s3')
